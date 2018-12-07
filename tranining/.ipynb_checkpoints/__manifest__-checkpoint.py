929025a8107ffb840392fd2741434934ab6f2900
Yan Zhang <huayiizhang@gmail.com>
2018-12-07 10:41:49 +0000

    test

--- tranining/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ tranining/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -10,13 +10,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Huayii Zhang",
+    'website': "http://www.nidaili.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
